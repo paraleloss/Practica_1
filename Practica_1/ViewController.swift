1c70f8b5159ed997934017a0a341e3f47b8ac848
Saul <saul.riot@gmail.com>
2021-12-20 08:47:35 -0600

    Initial Commit

---
+++ Practica_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica_1
+//
+//  Created by Saúl Pérez on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
